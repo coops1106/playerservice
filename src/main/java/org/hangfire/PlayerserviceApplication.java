63aee8db687e4967efd4b56e8dd33f36b7804e0c
dave.cooper <dave.cooper@obillex.com>
2016-07-07 08:32:04 +0000

    Initial commit

---
+++ src/main/java/org/hangfire/PlayerserviceApplication.java
@@ -0,0 +1,12 @@
+package org.hangfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerserviceApplication.class, args);
+	}
+}
